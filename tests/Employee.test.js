5156ebec40ad3bd65a2eca0b5f0883987e99a3fd
Liam Ok <liamokane19@gmail.com>
2022-02-09 21:24:59 +1100

    installed jest and added the infromation to the package.json file. I have also begun creating tests to confirm the value of employee. Currently it's calling name, id, email and office number. 4 failed. YAY! something is working now i can look into what it is

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -10,4 +10,26 @@ describe('employeeResult', () => {
         expect(employee.email).toBe('britney@mail.com');
         expect(employee.officeNumber).toBe('678');
     });
+    describe('nameCheck', () =>{
+        it('should call the getname() value from employee.js', () => {
+    
+            expect(employee.getname()).toBe('liam');
+        })
+    })
+    describe('IDCheck', () =>{
+        it('should call the getid() value from employee.js', () =>{
+            expect(employee.getid()).toBe('1234');
+        })
+    })
+    describe('EmailCheck', () =>{
+        it('should call the getemail() value from employee.js', () =>{
+            expect(employee.getemail()).toBe('britney@mail.com');
+        })
+    })
+    describe('EmailCheck', () =>{
+        it('should call the getofficeNumber() value from employee.js', () =>{
+            expect(employee.getofficeNumber()).toBe('678');
+        })
+    })
+
 });
