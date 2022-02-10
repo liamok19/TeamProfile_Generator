85d150d514a87d1c7a1984389ce512f02f6dcd31
Liam Ok <liamokane19@gmail.com>
2022-02-10 13:41:45 +1100

    VS code recommended that I had an empty array for the groupmembers at the start of the code. Which makes sense because we need to begin with nothing an then once we populate the fields from the prompt windows then I need to call 'groupMembers' again

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Manager = require('./tests/Manager.test');
 const Engineer = require('./tests/Engineer.test');
 const Intern = require('./tests/Intern.test');
 const path = require('path');
-const { group } = require('console');
+const groupMembers = [];
 
 const promptManager = function () {
     return inquirer.prompt ([
@@ -92,6 +92,68 @@ const prompList = () => {
     ])
 }
 
+//if selected during the promptList then the user will need to fill out the following question to add a new Engineer
+const promptEngineer = function () {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            message:'(Required) Whats is your name?', 
+            name: 'name',
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log('You will need to enter your name, pretty please');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message:'(Required) Whats is your employee ID?', 
+            name: 'employeeID',
+            validate: employeeID => {
+                if (employeeID){
+                    return true;
+                } else {
+                    console.log('You will need to enter your employeeID, pretty please');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message:'(Required) Whats is your email address?', 
+            name: 'emailAddress',
+            validate: emailAddress => {
+                if (emailAddress){
+                    return true;
+                } else {
+                    console.log('You will need to enter your  email address, pretty please');
+                    return false;
+                }
+            }
+        },         
+        {
+            type: 'input',
+            message:'(Required) Whats is your Github Username?', 
+            name: 'githubUsername',
+            validate: githubusername => {
+                if (githubusername){
+                    return true;
+                } else {
+                    console.log('You will need to enter your Github Username, pretty please');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(results => {
+        const engineer = new Engineer(results.name, results.employeeID, results.emailAddress, results.githubUsername);
+        groupMembers.push(engineer);
+        prompList();
+    })
+}
 
 
 promptManager();
