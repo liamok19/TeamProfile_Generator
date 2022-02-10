687052f57a0cca46d19bd22944386ee8ed6c3632
Liam Ok <liamokane19@gmail.com>
2022-02-10 20:26:52 +1100

    fixed issue in index.js before it was pulling in the test file which is why the manager file was undefined when being called for.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Manager = require('./tests/Manager.test');
-const Engineer = require('./tests/Engineer.test');
-const Intern = require('./tests/Intern.test');
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
 const path = require('path');
 const groupMembers = [];
 
@@ -76,20 +76,19 @@ const prompList = () => {
             message: '(Required) Select from the List below what employee you"d like to create or select finish Team Build',
             choices: ['Include team member: Engineer', 'Include team member: Intern', 'Finished team build - select here'], 
         }
-        .then (userChoice => {
+    ])
+        .then(userChoice => {
             switch (userChoice.list){
                 case 'Include team member: Engineer': 
                 promptEngineer();
                 break
-                case 'Include team member: Intern': 
+                case 'Include team member: Intern':
                 promptIntern();
                 break
-                case 'Finished team build - select here';
+                case 'Finished team build - select here':
                 finishedTeambuild();
             } 
         })
-
-    ])
 }
 
 //if selected during the promptList then the user will need to fill out the following question to add a new Engineer
@@ -155,5 +154,73 @@ const promptEngineer = function () {
     })
 }
 
+//if selected during the promptList then the user will need to fill out the following question to add a new intern
+const promptIntern = function () {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            message:'(Required) Whats is your name?', 
+            name: 'name',
+            validate: nameInput => {
+                if (nameInput){
+                    return true;
+                } else {
+                    console.log('You will need to enter your name, pretty please');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message:'(Required) Whats is your employee ID?', 
+            name: 'employeeID',
+            validate: employeeID => {
+                if (employeeID){
+                    return true;
+                } else {
+                    console.log('You will need to enter your employeeID, pretty please');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message:'(Required) Whats is your email address?', 
+            name: 'emailAddress',
+            validate: emailAddress => {
+                if (emailAddress){
+                    return true;
+                } else {
+                    console.log('You will need to enter your  email address, pretty please');
+                    return false;
+                }
+            }
+        },         
+        {
+            type: 'input',
+            message:'(Required) Whats school do you attend too?', 
+            name: 'school',
+            validate: school => {
+                if (school){
+                    return true;
+                } else {
+                    console.log('You will need to enter the school this intern goes too, pretty please');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(results => {
+        const intern = new Intern(results.name, results.employeeID, results.emailAddress, results.school);
+        groupMembers.push(intern);
+        prompList();
+    })
+}
+
+//If the user selects the finish team build option listed they're prompted with the following
+const finishedTeambuild = function () {
+    fs.writeFileSync(outputPath, generateSite(groupMembers), "utf-8");
+
+}
 
 promptManager();
